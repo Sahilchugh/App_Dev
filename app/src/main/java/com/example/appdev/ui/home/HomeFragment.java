6b0ef871c961ed34024cb58867835e62d93778aa
Sahil Chugh <sahilchugh88@gmail.com>
2020-06-23 17:05:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/appdev/ui/home/HomeFragment.java
@@ -0,0 +1,30 @@
+package com.example.appdev.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.appdev.MainActivity;
+import com.example.appdev.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final Button login=root.findViewById(R.id.btnAuth);
+
+        return root;
+    }
+}
